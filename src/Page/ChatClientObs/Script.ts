6ef5eca6d52f699e374d7228ffc46167e13154d4
iwatendo <iwatendo@gmail.com>
2023-01-10 20:55:53 +0900

    Twimojiを使用しないように修正

--- src/Page/ChatClientObs/Script.ts
+++ src/Page/ChatClientObs/Script.ts
@@ -11,7 +11,7 @@ let db = new Home.DB();
 
 db.Connect(() => {
     let server = new ChatClientController(true);
-    server.UseTwemoji = true;
+    server.UseTwemoji = false;
     server.IsEveryTimeScrolling = true;
     server.SwPeer = new SWPeer(server, LinkUtil.GetPeerID(), null);
 });
